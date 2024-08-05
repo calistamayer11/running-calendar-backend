8af8ee0091865cf977bebae117504312e8fd2885
Calista Mayer <calistammayer@gmail.com>
2024-08-04 20:26:45 -0400

    testing prompts

--- main.py
+++ main.py
@@ -13,7 +13,27 @@ completion = client.chat.completions.create(
     messages=[
         {
             "role": "system",
-            "content": "You are a cross country coach. You are providing to the users a running mileage training plan.",
+            "content": """You are a cross country coach. You are providing to the users a running mileage training plan.
+            Please give responses as a JSON array of events such as the following:
+            [
+                {
+                title  : 'event1',
+                start  : '2010-01-01'
+                },
+                {
+                title  : 'event2',
+                start  : '2010-01-05',
+                end    : '2010-01-07'
+                },
+                {
+                title  : 'event3',
+                start  : '2010-01-09T12:30:00',
+                allDay : false // will make the time show
+                }
+            ]
+            
+            
+            """,
         },
         {"role": "user", "content": "Give me a training plan."},
     ],
